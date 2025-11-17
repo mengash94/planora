801dd8e0759ff70fdb940b394c1c36a41f888cb1
mengash94 <67413443+mengash94@users.noreply.github.com>
2025-11-17 15:35:50 +0200

    Ensure opaque system bars on Android

--- android/app/src/main/java/net/planora/app/MainActivity.java
+++ android/app/src/main/java/net/planora/app/MainActivity.java
@@ -3,7 +3,6 @@ package net.planora.app;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.webkit.CookieManager;
 
 import androidx.core.content.ContextCompat;
 import androidx.core.view.WindowCompat;
@@ -11,12 +10,16 @@ import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    // @Override
-    // public void onCreate(Bundle savedInstanceState) {
-    //     // super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    //     // CookieManager cm = CookieManager.getInstance();
-    //     // cm.setAcceptCookie(true);
-    //     // cm.setAcceptThirdPartyCookies(getBridge().getWebView(), true);
-    // }
+        // מונע מצב של edge-to-edge שקוף ומשחזר צבעי מערכת קבועים
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        WindowCompat.setDecorFitsSystemWindows(window, true);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.status_bar));
+        window.setNavigationBarColor(ContextCompat.getColor(this, R.color.navigation_bar));
+    }
 }
